d6982c3c42d6f4efc55316d28155fbf60f78b1ff
mzmpy <2316687327@qq.com>
2023-02-10 15:34:08 +0000

    Deploying to static-pages from @ mzmpy/mynotes-es@1ba5905bdc478c42c157b9d47c470295533205d0 🚀

---
+++ chunks/js/test-LJJFLLBW.js
@@ -0,0 +1,32 @@
+import {
+  ElInput
+} from "./chunk-GQIHCBJX.js";
+import {
+  Fragment,
+  defineComponent,
+  h,
+  init_vue_jsxImportSource,
+  init_vue_runtime_esm_bundler,
+  ref
+} from "./chunk-2QG72R3B.js";
+
+// src/views/test/index.js
+init_vue_jsxImportSource();
+init_vue_runtime_esm_bundler();
+console.log("TestView");
+var test_default = defineComponent({
+  name: "TestView",
+  components: { ElInput },
+  setup(props, ctx) {
+    const text = ref("");
+    const onInput = (val) => {
+      text.value = val;
+    };
+    return () => {
+      return /* @__PURE__ */ h(Fragment, null, /* @__PURE__ */ h("div", null, "TestView ", text.value), /* @__PURE__ */ h(ElInput, { style: { width: "300px!important" }, modelValue: text.value, onInput, placeholder: "Hello" }));
+    };
+  }
+});
+export {
+  test_default as default
+};
